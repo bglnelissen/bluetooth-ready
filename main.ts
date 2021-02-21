3d801d0d1bf15972ea189c217f7cca129c44c686
Bastiaan Nelissen <bglnelissen@users.noreply.github.com>
2021-02-21 17:34:35 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showString("-")
+bluetooth.setTransmitPower(9)
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
+bluetooth.startTemperatureService()
+bluetooth.startLEDService()
+bluetooth.startMagnetometerService()
+basic.showString("+")
+basic.forever(function () {
+	
+})
